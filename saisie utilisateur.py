618122e19f9fbb5cedcd95f00e614d9510d195b1
Sakina <macbookpro@MacBooks-MacBook-Pro.local>
2025-11-30 00:46:06 +0000

    first commit

---
+++ saisie utilisateur.py
@@ -0,0 +1,14 @@
+"""
+interagir avec l'utilisateur pour obtenir des entrées
+
+"""
+
+
+# question = input("Poser une question : ")
+
+# print("Votre question est : "+ question)
+
+# nom = input("Quel est votre nom ? ")
+# print("Bonjour " + nom + " !")
+age = input("Quel age avez-vous ? ")
+print("Vous avez " + age + " ans.")
---
+++ variables.py
@@ -0,0 +1,26 @@
+
+"""
+ integer        nombre entier
+ string             chaine de caractere
+ type()          permet de connaitre le type d'une variable
+ float            nombre a virgule
+ boolean       valeur vrai ou faux
+ """
+ 
+ 
+
+age = 25
+print(age)
+
+prenom = "Alice"
+print(prenom)
+
+prenom = "Bob"
+print(type (prenom))
+
+taille = 1.75
+print(taille)
+print(type (taille))
+
+masculin = True
+print(masculin)
